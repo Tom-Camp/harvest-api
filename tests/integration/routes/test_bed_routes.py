25730f49547af2c1c71b756309515d58ed384130
Tom Camp <tom.camp@civicactions.com>
2025-09-26 15:35:54 -0400

    mypy fixes for test_bed

--- tests/integration/routes/test_bed_routes.py
+++ tests/integration/routes/test_bed_routes.py
@@ -31,19 +31,20 @@ class TestBedRoutes:
         headers = await get_auth_headers(client=client, user_name=username)
 
         garden = default_gardens.get(user_name)
-        if isinstance(garden, Garden):
-            response = await client.post(
-                url="/api/beds",
-                json={
-                    "name": "The main bed",
-                    "description": "This is the garden",
-                    "garden_id": garden.id,
-                },
-                headers=headers,
-            )
-            assert response.status_code == expected_status
-        else:
-            pytest.fail(f"No garden found for user {username}")
+        garden_id = garden.id if isinstance(garden, Garden) else ""
+        response = await client.post(
+            url="/api/beds",
+            json={
+                "name": f"The main bed for {username}",
+                "description": "This is the garden",
+                "garden_id": str(garden_id),
+            },
+            headers=headers,
+        )
+        new_bed = response.json()
+        assert response.status_code == expected_status
+        if response.status_code == 200:
+            assert new_bed.get("name") == f"The main bed for {username}"
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize(
@@ -66,14 +67,13 @@ class TestBedRoutes:
         test_as = default_user.get(user_name, "")
         username = test_as.username if isinstance(test_as, User) else ""
         headers = await get_auth_headers(client=client, user_name=username)
-        garden = default_gardens.get(user_name)
+        garden = default_gardens.get("authenticated")
         if isinstance(garden, Garden):
             response = await client.get(url=f"/api/beds/{garden.id}", headers=headers)
             assert response.status_code == expected_status
-            assert isinstance(list, response.json())
+            assert isinstance(response.json(), list)
         else:
-            pytest.fail(f"No garden found for user {username}")
-
+            pytest.fail("No garden found for user authenticated")
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize(
@@ -97,10 +97,12 @@ class TestBedRoutes:
         headers = await get_auth_headers(client=client, user_name=username)
 
         garden = default_gardens.get("tester", "")
-        bed = garden.beds[0]
+        if isinstance(garden, Garden):
+            bed = garden.beds[0]
+            response = await client.get(
+                url=f"/api/beds/{garden.id}/{bed.id}", headers=headers
+            )
 
-        response = await client.get(
-            url=f"/api/beds/{garden.id}/{bed.id}", headers=headers
-        )
-
-        assert response.status_code == expected_status
+            assert response.status_code == expected_status
+        else:
+            pytest.fail("No garden found for user tester")
