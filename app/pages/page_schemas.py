b081d740a303c130856c4e25edc09ec86a972bda
Tom Camp <tom.camp@civicactions.com>
2025-09-29 13:19:37 -0400

    Updating Pages.

--- app/pages/page_schemas.py
+++ app/pages/page_schemas.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class PageRead(BaseModel):
@@ -10,20 +10,29 @@ class PageRead(BaseModel):
     title: str
     body: str
     created_date: datetime
+    updated_date: datetime
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class PageList(BaseModel):
     id: UUID
-    user_id: UUID
     title: str
     created_date: datetime
+    updated_date: datetime
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class PageCreate(BaseModel):
     title: str
     body: str
 
+    model_config = ConfigDict(from_attributes=True)
+
 
 class PageUpdate(BaseModel):
     title: str | None = None
     body: str | None = None
+
+    model_config = ConfigDict(from_attributes=True)
