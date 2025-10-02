aced673b0f51b782ff5623920a2073fa8a2df08e
Tom Camp <tomcamp@protonmail.ch>
2025-10-02 08:59:09 -0400

    Standardizing note tests.

--- tests/integration/routes/test_bed_notes/test_bed_notes_delete_routes.py
+++ tests/integration/routes/test_bed_notes/test_bed_notes_delete_routes.py
@@ -5,10 +5,12 @@ from httpx import AsyncClient
 
 from app.gardens.garden_models import Garden
 from app.users.user_models import User
-from tests.helpers.test_helpers import get_auth_headers
+from tests.helpers.test_helpers import create_note, get_auth_headers
 
 
 class TestBedNoteDeleteRoutes:
+    note_json: dict = {"note": "this is a bed note", "bed_id": ""}
+    note_url: str = "/api/bed-notes/"
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize(
@@ -33,17 +35,55 @@ class TestBedNoteDeleteRoutes:
         create_headers = await get_auth_headers(client=client, user_name=username)
         garden = default_gardens.get("tester")
         if isinstance(garden, Garden):
-            bed_id = garden.beds[0].id
-            create_response = await client.post(
-                url="/api/bed-notes/",
-                json={"note": "My new bed note", "bed_id": str(bed_id)},
+            self.note_json["bed_id"] = str(garden.beds[0].id)
+            note = await create_note(
+                client=client,
+                note=self.note_json,
+                url=self.note_url,
                 headers=create_headers,
             )
-            note = create_response.json()
 
             test_as = default_user.get(user_name, "")
             username = test_as.username if isinstance(test_as, User) else ""
             headers = await get_auth_headers(client=client, user_name=username)
+
+            delete_note = await client.delete(
+                url=f"/api/bed-notes/{note.get('id')}", headers=headers
+            )
+            assert delete_note.status_code == expected_status
+
+        else:
+            pytest.fail("No garden found for user tester")
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "user_name,expected_status",
+        [
+            ("admin", 200),
+            ("moderator", 200),
+            ("authenticated", 200),
+        ],
+    )
+    async def test_delete_bed_note_own(
+        self,
+        client: AsyncClient,
+        default_user: dict[str, User],
+        default_gardens: dict[str, Garden],
+        user_name: str,
+        expected_status: int,
+    ):
+        create_as = default_user.get(user_name, "")
+        username = create_as.username if isinstance(create_as, User) else ""
+        headers = await get_auth_headers(client=client, user_name=username)
+        garden = default_gardens.get(user_name)
+        if isinstance(garden, Garden):
+            self.note_json["bed_id"] = str(garden.beds[0].id)
+            note = await create_note(
+                client=client,
+                note=self.note_json,
+                url=self.note_url,
+                headers=headers,
+            )
 
             delete_note = await client.delete(
                 url=f"/api/bed-notes/{note.get('id')}", headers=headers
