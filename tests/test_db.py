2ca3aae0c0bb3de20aed4b229d26559fd7828290
Tom Camp <tomcamp@protonmail.ch>
2025-10-13 17:32:03 -0400

    Updating conftest.

---
+++ tests/test_db.py
@@ -0,0 +1,27 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlmodel import SQLModel
+
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///:memory:"
+
+engine: AsyncEngine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    echo=False,
+    future=True,
+)
+
+TestingSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with TestingSessionLocal() as session:
+        yield session
+
+
+metadata = SQLModel.metadata
